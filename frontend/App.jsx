fc811685b1c86f5c22ffd6832624be291e276c31
Carlos Martinez <martinezcj2@gmail.com>
2016-12-11 19:35:43 -0500

    made react routes

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -6,7 +6,11 @@ import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 import Navbar from './components/Navbar';
 import BlogPosts from './components/BlogPosts';
 import CreatePost from './components/CreatePost';
-import Login from "./components/login";
+import Login from "./components/Login";
+import Classes from "./components/Classes";
+import Calendar from "./components/Calendar";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
 
 const App = React.createClass({
   render: function(){
@@ -24,7 +28,15 @@ const App = React.createClass({
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={BlogPosts}/>
+      <IndexRoute component={BlogPosts} />
+      <Route path="/about" component={About}></Route>
+      <Route path="/classes" component={Classes}></Route>
+      <Route path="/calendar" component={Calendar}></Route>
+      <Route path="/gallery" component={Gallery}></Route>
+      <Route path="/create-post" component={CreatePost}></Route>
+      <Route path="/login" component={Login}></Route>
+      {/* <Route path="/sign-out" onEnter={}></Route> */}
+      <Route path="/contact" component={Contact}></Route>
     </Route>
   </Router>,
   document.getElementById('root')
---
+++ frontend/components/Classes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Classes = React.createClass({
+  render: function(){
+    return (
+      <div>This is the Classes Page</div>
+    )
+  }
+})
+
+export default Classes;
